79f8ada0408109271a67a47993a14086a8984f8f
Maxim Zhukov <crmaxx@gmail.com>
2015-03-03 19:05:24 +0300

    Initial commit

---
+++ src/main/java/ru/gravenet/downloader/Main.java
@@ -0,0 +1,10 @@
+package ru.gravenet.downloader;
+
+/**
+ * Created by Maxim Zhukov <crmaxx@ya.ru> on 2015-03-03.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
